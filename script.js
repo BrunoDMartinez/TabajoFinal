25b3a83db3958887a0b6f98bc6a1a84021bcbf90
BrunoDMartinez <1999martinez.bruno@gmail.com>
2023-08-16 19:38:09 -0300

    juego terminado

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let deposito2 = document.querySelector("#a2");
 let imgArrastrada2 = document.querySelector("#d2");
 
 imgArrastrada.addEventListener("dragstart", (event) => {
-    event.dataTransfer.setData("Text1", "./imagenes/Rompe1.png");
+    event.dataTransfer.setData("Text", "./imagenes/Rompe1.png");
 });
 
 deposito.addEventListener("dragover", (event) => {
@@ -26,11 +26,14 @@ deposito.addEventListener("drop", (event) => {
     event.preventDefault();
     let imgGuardada = event.dataTransfer.getData("Text");
     deposito.innerHTML = `<img src="${imgGuardada}" alt="Imagen soltada">`;
+});
+
+imgArrastrada.addEventListener("dragend", () => {
     imgArrastrada.classList.add("desaparece");
 });
 
 imgArrastrada1.addEventListener("dragstart", (event) => {
-    event.dataTransfer.setData("Text2", "./imagenes/Rompe3.png");
+    event.dataTransfer.setData("Text", "./imagenes/Rompe3.png");
 });
 
 deposito1.addEventListener("dragover", (event) => {
@@ -41,20 +44,33 @@ deposito1.addEventListener("drop", (event) => {
     event.preventDefault();
     let imgGuardada1 = event.dataTransfer.getData("Text");
     deposito1.innerHTML = `<img src="${imgGuardada1}" alt="Imagen soltada">`;
-    imgArrastrada1.classList.add("desaparece");
+});
+
+imgArrastrada1.addEventListener("dragend", () => {
+    imgArrastrada1.classList.add("desaparece"); 
 });
 
 imgArrastrada2.addEventListener("dragstart", (event) => {
-    event.dataTransfer.setData("Text3", "./imagenes/rompe2.png");
+    event.dataTransfer.setData("Text", "./imagenes/rompe2.png");
 });
 
 deposito2.addEventListener("dragover", (event) => {
     event.preventDefault();
 });
 
+
 deposito2.addEventListener("drop", (event) => {
     event.preventDefault();
     let imgGuardada2 = event.dataTransfer.getData("Text");
     deposito2.innerHTML = `<img src="${imgGuardada2}" alt="Imagen soltada">`;
+});
+
+imgArrastrada2.addEventListener("dragend", () => {
     imgArrastrada2.classList.add("desaparece");
+});
+
+let botonReiniciar = document.getElementById("botonReiniciar");
+
+botonReiniciar.addEventListener("click", () => {
+    window.location.reload(); // Recargar la página
 });
